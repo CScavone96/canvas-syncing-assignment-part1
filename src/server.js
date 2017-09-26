8c021af5629c04cf1523d2bed898649883a9d855
Clayton <cscavone96@gmail.com>
2017-09-25 22:40:27 -0400

    fixed

--- src/server.js
+++ src/server.js
@@ -7,25 +7,25 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const index = fs.readFileSync(`${__dirname}/../client/index.html`);
 
 const onRequest = (request, response) => {
-    response.writeHead(200, { 'ContentType': 'text/html' });
-    response.write(index);
-    response.end();
+  response.writeHead(200, { ContentType: 'text/html' });
+  response.write(index);
+  response.end();
 };
 
 const app = http.createServer(onRequest).listen(port);
 
-console.log(`Listening on 127.0.0.1: ${port}`)
+console.log(`Listening on 127.0.0.1: ${port}`);
 
 const io = socketio(app);
 
 let myNum = 0;
 
 io.sockets.on('connection', (socket) => {
-    console.log('started');
-    socket.on('serverUpdate', (data) => {
-        myNum += data.message;
-        io.emit('updateValue', { value: myNum });
-    });
+  console.log('started');
+  socket.on('serverUpdate', (data) => {
+    myNum += data.message;
+    io.emit('updateValue', { value: myNum });
+  });
 });
 
 console.log('Websocket server started');
